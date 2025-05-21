9537f24fd87c7a7b2bcdaa81958d569a274a7a97
patrick <patrick@morph.co.uk>
2025-05-21 13:26:26 +0100

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>ABOUT!</h1>
+    <p>This is the about page.</p>
+    <NuxtLink to="/">INDEX page</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to my Nuxt 3 app!</h1>
+    <p>This is the home page.</p>
+<NuxtLink to="/about">About page</NuxtLink>  </div>
+</template>
