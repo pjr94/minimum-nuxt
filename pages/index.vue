4a165cc1f4dd5183a2c59138cc80bdbe91df4734
patrick <patrick@morph.co.uk>
2025-05-21 13:42:55 +0100

    add additional page

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,7 @@
   <div>
     <h1>Welcome to my Nuxt 3 app!</h1>
     <p>This is the home page.</p>
-<NuxtLink to="/about">About page</NuxtLink>  </div>
+    <NuxtLink to="/about">About page</NuxtLink>
+    <NuxtLink to="/taking-part">Taking part page</NuxtLink>
+  </div>
 </template>
